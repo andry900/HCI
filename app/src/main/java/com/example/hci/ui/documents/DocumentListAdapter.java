3dfb653c8e588e09e5815c2907dfd5f7772c33da
stefanosantinelli <stefano_santinelli@yahoo.com>
2020-05-29 15:36:10 +0200

    adapter for documents recycle view creation layout for document fragment ended minors update to documents fragment

---
+++ app/src/main/java/com/example/hci/ui/documents/DocumentListAdapter.java
@@ -0,0 +1,61 @@
+package com.example.hci.ui.documents;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.hci.R;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DocumentListAdapter extends RecyclerView.Adapter<DocumentListAdapter.MyViewHolder>{
+
+    private final ArrayList<DocumentsFragment.Document> data;
+
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView label;
+        TextView status;
+
+        MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name =  itemView.findViewById(R.id.doc_name);
+            status = itemView.findViewById(R.id.doc_status);
+            label = itemView.findViewById(R.id.doc_label);
+        }
+    }
+
+    DocumentListAdapter(ArrayList<DocumentsFragment.Document> savedDocs) {
+        data = savedDocs;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.doc_item, parent, false);
+
+        return new DocumentListAdapter.MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        if (data.get(position) != null) {
+            (holder.name).setText(data.get(position).getName());
+            (holder.status).setText(data.get(position).getStatus());
+            (holder.label).setText(data.get(position).getLabel());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+}
--- app/src/main/java/com/example/hci/ui/documents/DocumentsFragment.java
+++ app/src/main/java/com/example/hci/ui/documents/DocumentsFragment.java
@@ -1,21 +1,116 @@
 package com.example.hci.ui.documents;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hci.R;
 
+import java.util.ArrayList;
+
 public class DocumentsFragment extends Fragment {
+
+    private RecyclerView docList;
+    private LinearLayoutManager lm;
+    private DocumentListAdapter mAdapter;
+    private ArrayList<Document> savedDocs;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_documents, container, false);
 
+        docList = root.findViewById(R.id.docs_list);
+
+        savedDocs = new ArrayList<>();
+        savedDocs.add(new Document("February Energy Bill", "bill","payed",
+                null,null,null));
+        savedDocs.add(new Document("February telephone Bill", "bill","payed",
+                null,null,null));
+        savedDocs.add(new Document("telephone contract", "contract","payed",
+                null,null,null));
+
+        docList.setHasFixedSize(true);
+        lm = new LinearLayoutManager(getContext());
+        docList.setLayoutManager(lm);
+        mAdapter = new DocumentListAdapter(savedDocs);
+        docList.setAdapter(mAdapter);
         return root;
+    }
+
+    public static class Document{
+
+        private String name;
+        private String label;
+        private String status;
+        private String img;
+        private String utility;
+        private Float amount;
+
+        Document(String name, String label, String status, String img, String utility, Float amount) {
+            this.name = name;
+            this.label = label;
+            this.status = status;
+            this.img = img;
+            this.utility = utility;
+            this.amount = amount;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        String getLabel() {
+            return label;
+        }
+
+        public void setLabel(String label) {
+            this.label = label;
+        }
+
+        String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getImg() {
+            return img;
+        }
+
+        public void setImg(String img) {
+            this.img = img;
+        }
+
+        public String getUtility() {
+            return utility;
+        }
+
+        public void setUtility(String utility) {
+            this.utility = utility;
+        }
+
+        public Float getAmount() {
+            return amount;
+        }
+
+        public void setAmount(Float amount) {
+            this.amount = amount;
+        }
+
+
     }
 }
